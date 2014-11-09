504100a998436d0d48c9fd970813411b2beafec3
YaronCohen212 <y21287@gmail.com>
2014-11-09 23:56:53 +0200

    Task 2

--- src/il/ac/mta/Test1Servlet.java
+++ src/il/ac/mta/Test1Servlet.java
@@ -7,7 +7,8 @@ import javax.servlet.http.*;
 public class Test1Servlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
+		int num1=3, num2=4, num3=7;
 		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world is yaron cohen");
+		resp.getWriter().println("Result of (" + num1 + "+" + num2 +")*" + num3 + "=" + (num1+num2)*num3);
 	}
 }
