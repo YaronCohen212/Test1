169abd3f308995cbd83351b3d1c502be447b0544
YaronCohen212 <y21287@gmail.com>
2014-11-16 23:14:06 +0200

    first try ex 3

--- src/il/ac/mta/Test1Servlet.java
+++ src/il/ac/mta/Test1Servlet.java
@@ -7,9 +7,11 @@ import javax.servlet.http.*;
 public class Test1Servlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
+		
 		int num1=3, num2=4, num3=7,res;
 		res=(num1+num2)*num3;
 		resp.setContentType("text/html");
 		resp.getWriter().println("<h1>Result of (" + num1 + "+" + num2 +")*" + num3 + "=" + res + "</h1>");
+		
 	}
 }
