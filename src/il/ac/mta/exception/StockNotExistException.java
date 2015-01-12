1d459397e6b3a4077a39e6584c7220d877919bd6
YaronCohen212 <y21287@gmail.com>
2015-01-12 16:19:28 +0200

    add exceptions

---
+++ src/il/ac/mta/exception/StockNotExistException.java
@@ -0,0 +1,10 @@
+package il.ac.mta.exception;
+
+public class StockNotExistException extends Exception{
+
+	private static final long serialVersionUID = 1L;
+	
+	public StockNotExistException(String symbol) {
+		super("Stock " + symbol + " not exists!");
+	}
+}
